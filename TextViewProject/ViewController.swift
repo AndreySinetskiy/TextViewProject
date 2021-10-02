8449e1963c2d542c5a85c66c8e48f70a831273c5
Andrey Sinetskiy <siniy.85@mail.ru>
2021-10-02 14:07:04 +0300

    Initial Commit

---
+++ TextViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextViewProject
+//
+//  Created by 1 on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
